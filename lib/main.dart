ed11858d066854064abf43650a432775e6843847
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-05-21 11:40:15 +0530

    undestand classes

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,36 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 233, 187, 3),
-                Color.fromARGB(255, 59, 0, 91),
-                Color.fromRGBO(0, 234, 255, 1),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 233, 187, 3),
+            Color.fromARGB(255, 114, 24, 163),
+            Color.fromRGBO(0, 234, 255, 1),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello worlds!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
 }
